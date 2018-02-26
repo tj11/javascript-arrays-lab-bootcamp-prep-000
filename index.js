6790ee4ff49260ff2bd874492452654c8539504d
tj11 <tedwardj11@gmail.com>
2018-02-26 18:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten(name) {
 }
 
 function appendKitten(name) {
-  var newKittens = [name,...kittens]
+  var newKittens = [...kittens,name]
   return newKittens
 }
 
