9697f9ee9c550b6ddcb18c0867c6de86c7f30875
tj11 <tedwardj11@gmail.com>
2018-02-26 18:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+
+
+
